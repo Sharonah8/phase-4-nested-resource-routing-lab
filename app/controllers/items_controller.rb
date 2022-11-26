793dd29752a6c8842328527641a38c7bfab0aa38
Sharonah8 <nyamongosharon88@gmail.com>
2022-11-26 20:17:31 +0300

    Go back, incomplete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create!(item_params)
+    render json: item, status: :created
   end
 
 end
